3e434173a1dc07ba3b0cbb22df67c12e391bed68
Oishi106l <mahmudaoishi457@gmail.com>
2026-05-03 23:22:47 +0600

    updated

--- src/Routes/PrivateRoutes.jsx
+++ src/Routes/PrivateRoutes.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Navigate, useLocation } from 'react-router-dom'
 import { useAuth } from '../context/AuthProvider'
 
-const PrivateRoutes = ({ children }) => {
+const PrivateRoutes = ({ children }) => {           
   const { user, loading } = useAuth()
   const location = useLocation()
 
